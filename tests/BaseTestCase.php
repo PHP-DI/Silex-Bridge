68557acfff557d8ed470883d1a84f5f6738338a4
jdreesen <jacob@hdreesen.de>
2016-02-19 20:11:33 +0100

    Make this bridge compatible to Silex 1 and 2
    Because they use different, incompatible versions of Pimple some ugly hacks are needed for this.

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -8,12 +8,22 @@ use PHPUnit_Framework_TestCase;
 
 class BaseTestCase extends PHPUnit_Framework_TestCase
 {
+    public static function isSilex1(\Silex\Application $app)
+    {
+        return ! self::isSilex2($app);
+    }
+
+    public static function isSilex2(\Silex\Application $app)
+    {
+        return is_subclass_of($app, 'Pimple\Container', false);
+    }
+
     protected function createApplication(ContainerBuilder $builder = null)
     {
         $app = new Application($builder);
 
         $app['debug'] = true;
-        $app['exception_handler']->disable();
+        unset($app['exception_handler']);
 
         return $app;
     }
