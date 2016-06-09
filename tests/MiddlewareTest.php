70a407ecc01d3566051094f996f5ea8b72a9ed7a
Jacob Dreesen <jacob@hdreesen.de>
2016-06-09 22:53:31 +0200

    Allow short circuiting the controller in a before route middleware
    This brings the bridge in line with the behavior of Silex.

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -99,4 +99,35 @@ class MiddlewareTest extends BaseTestCase
         $response = $application->handle($request);
         $this->assertEquals('Hello john', $response->getContent());
     }
+
+    /**
+     * @test
+     */
+    public function should_allow_short_circuiting_the_controller_in_before_route_middleware()
+    {
+        $application = $this->createApplication();
+        $request = Request::create('/');
+
+        $shouldBeCalled = $this->createSpyCallback();
+        $shouldBeCalled->expects($this->once())->method('__invoke');
+
+        $shouldNotBeCalled = $this->createSpyCallback();
+        $shouldNotBeCalled->expects($this->never())->method('__invoke');
+
+        // route middleware
+        $application
+            ->get('/', $shouldNotBeCalled)
+            ->before(function () {
+                return new Response('Expected result');
+            })
+            ->before($shouldNotBeCalled)
+            ->after($shouldBeCalled);
+
+        $response = $application->handle($request);
+        $this->assertEquals('Expected result', $response->getContent());
+    }
+
+    private function createSpyCallback() {
+        return $this->getMockBuilder('stdClass')->setMethods(['__invoke'])->getMock();
+    }
 }
