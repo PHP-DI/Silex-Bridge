af3ff2e7b31c22a21b3d30f731d745cca5a6c351
Jacob Dreesen <jacob@hdreesen.de>
2016-06-09 23:16:16 +0200

    Fix short circuiting the controller in a before application middleware
    - Added missing use statement for the Response
    - Removed broken code in the finish middleware that tries to set the response
      (finish is called after the response has been sent)

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -103,6 +103,34 @@ class MiddlewareTest extends BaseTestCase
     /**
      * @test
      */
+    public function should_allow_short_circuiting_the_controller_in_before_application_middleware()
+    {
+        $application = $this->createApplication();
+        $request = Request::create('/');
+
+        $shouldBeCalled = $this->createSpyCallback();
+        $shouldBeCalled->expects($this->once())->method('__invoke');
+
+        $shouldNotBeCalled = $this->createSpyCallback();
+        $shouldNotBeCalled->expects($this->never())->method('__invoke');
+
+        $application->get('/', $shouldNotBeCalled);
+
+        // application middleware
+        $application->before(function () {
+            return new Response('Expected result');
+        });
+        $application->before($shouldNotBeCalled);
+        $application->after($shouldBeCalled);
+        $application->finish($shouldBeCalled);
+
+        $response = $application->handle($request);
+        $this->assertEquals('Expected result', $response->getContent());
+    }
+
+    /**
+     * @test
+     */
     public function should_allow_short_circuiting_the_controller_in_before_route_middleware()
     {
         $application = $this->createApplication();
